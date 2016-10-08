90ce84e78a9a69b8103e7d480ea15c384060263e
aayaresko <aayaresko@gmail.com>
2016-10-08 20:46:36 +0300

    few routes refactored.

--- src/app/tasks-board/tasks-routing.module.ts
+++ src/app/tasks-board/tasks-routing.module.ts
@@ -1,39 +1,41 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { TasksListComponent } from './tasks-list.component';
 import { TaskFormComponent } from './task-form.component';
 import { TaskResolveService } from './task-resolve.service';
 import { TasksBoardComponent } from './tasks-board.component';
 
-@NgModule({
-    imports: [
-        RouterModule.forChild([
+const routes: Routes = [
+    {
+        path: '',
+        component: TasksBoardComponent,
+        children: [
+            {
+                path: 'edit/:id',
+                component: TaskFormComponent,
+                resolve: {
+                    task: TaskResolveService
+                }
+            },
+            {
+                path: 'delete/:id',
+                component: TasksListComponent
+            },
+            {
+                path: 'create',
+                component: TaskFormComponent
+            },
             {
                 path: '',
-                component: TasksBoardComponent,
-                children: [
-                    {
-                        path: 'edit/:id',
-                        component: TaskFormComponent,
-                        resolve: {
-                            task: TaskResolveService
-                        }
-                    },
-                    {
-                        path: 'delete/:id',
-                        component: TasksListComponent
-                    },
-                    {
-                        path: 'create',
-                        component: TaskFormComponent
-                    },
-                    {
-                        path: '',
-                        component: TasksListComponent,
-                    },
-                ]
-            }
-        ])
+                component: TasksListComponent,
+            },
+        ]
+    }
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forChild(routes)
     ],
     exports: [
         RouterModule
